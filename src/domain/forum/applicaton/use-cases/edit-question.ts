04919a7c1eaab5a1bab884ca03897817a8379d47
Andre Grillo <andrell.grillo@gmail.com>
2024-01-25 17:31:04 -0300

    add edit question

---
+++ src/domain/forum/applicaton/use-cases/edit-question.ts
@@ -0,0 +1,38 @@
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+interface EditQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  title: string
+  content: string
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface EditQuestionUseCaseResponse {}
+
+export class EditQuestionUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    authorId,
+    questionId,
+    title,
+    content,
+    // attachmentsIds,
+  }: EditQuestionUseCaseRequest): Promise<EditQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId)
+    if (!question) {
+      throw new Error('Question not found')
+    }
+
+    if (authorId !== question.authorId.toValue()) {
+      throw new Error('Not Allowed')
+    }
+
+    question.title = title
+    question.content = content
+
+    await this.questionsRepository.save(question)
+    return {}
+  }
+}
