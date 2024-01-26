ed6347dde6ec97ba691e34ab03936b56f1e1b59c
Andre Grillo <andrell.grillo@gmail.com>
2024-01-26 14:42:49 -0300

    add question comment + class comment

---
+++ src/domain/forum/applicaton/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,33 @@
+import { InMemoryQuestionCommentsRepository } from '@/test/repositories/in-memory-question-comment-repository'
+import { CommentOnQuestionUseCase } from './comment-on-question'
+import { InMemoryQuestionsRepository } from '@/test/repositories/in-memory-questions-repository'
+import { makeQuestion } from '@/test/factories/make-question'
+
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Comment on Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+  it('should be able to comment on question', async () => {
+    const question = makeQuestion()
+    await inMemoryQuestionsRepository.create(question)
+
+    await sut.execute({
+      questionId: question.id.toString(),
+      authorId: question.authorId.toString(),
+      content: 'Comentário teste',
+    })
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual(
+      'Comentário teste',
+    )
+  })
+})
